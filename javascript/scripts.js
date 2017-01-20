21302a89e12b014119772e92d2267e0704dac959
NikitaSandstrom <NikitaSandstrom1@gmail.com>
2017-01-19 21:42:00 -0600

    Fixing Commits

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,28 +1,34 @@
 $(document).ready(function () {
+
     var $overlay = $('<div id="overlay"></div>');
-var $image = $('<img>');
+    var $image = $('<img>');
 
-$overlay.append($image);
-$("body").append($overlay);
+    $overlay.append($image);
+    $("body").append($overlay);
 
-$('.albumGallery').on('click', 'a', function (e) {
-    console.log("click");
+    $('.albumGallery').on('click', 'a', function (e) {
+        console.log("click");
 
-    var imageLocation = $(this).attr("href");
+        var imageLocation = $(this).attr("href");
 
-    $image.attr("src", imageLocation);
+        $image.attr("src", imageLocation);
 
-    $overlay.show();
+        $overlay.show();
 
-    e.preventDefault();
+        e.preventDefault();
 
-});
+    });
 
 
 
-$(document).on('click', function (e) {
-            if ($(e.target).has('img').length) {
-                $overlay.hide();
-            }
-        });
+    $(document).on('click', function (e) {
+        if ($(e.target).has('img').length) {
+            $overlay.hide();
+        }
+    });
+
+
+    $("#LinkedIn").click(function () {
+        alert("This User currently does not have a LinkedIn account, please check later in the year 2019.");
+    });
 });
